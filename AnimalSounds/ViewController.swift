7f21b65c75a3f0eeebabf2af92b7f2c968a3f5c7
John Green <jpgreen@Johns-iMac.local>
2018-07-23 16:58:41 -0400

    Part One

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
+    
+    
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
+    @IBAction func catButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Meow!"
+        meowSound.play()
+    }
+    
+    @IBAction func dogButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Woof!"
+        woofSound.play()
+    }
+    
+    @IBAction func cowButtonTapped(_ sender: UIButton) {
+        animalSoundLabel.text = "Moo!"
+        mooSound.play()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
